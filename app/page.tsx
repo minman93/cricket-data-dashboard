2d484b15819140f3d1b0464b90df62be5fcf06e0
minman93 <matt_j_inman@hotmail.co.uk>
2024-01-24 13:22:10 +0000

    latest commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { roboto } from "./ui/fonts";
 
 export default function Dashboard() {
   return (
-    <div>
-      <p className={`${roboto.className} text-2xl`}>
+    <div className="flex justify-around py-8 w-full h-full bg-gray-800">
+      <h1 className={`${roboto.className} text-4xl font-extrabold text-white`}>
         Welcome To Cricket Stats Dashboard!
-      </p>
+      </h1>
     </div>
   );
 }
