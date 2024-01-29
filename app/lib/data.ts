bdbfad78c485db4abeb8a1071a4dfe8c8acb97f7
minman93 <matt_j_inman@hotmail.co.uk>
2024-01-29 22:38:58 +0000

    changes

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -31,7 +31,9 @@ export async function fetchPlayers(): Promise<Player[]> {
   `;
 
     const players = await db.query(query);
+    console.log(players.rows);
     return players.rows as Player[];
+    
   } catch (error) {
     console.error("Database Error:", error);
     return [];
